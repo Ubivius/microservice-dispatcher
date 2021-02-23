5199ccc24401c11ac838e4c10af44fe2d5444d54
Malcolm St. John <malcolm19@hotmail.com>
2021-02-23 17:55:07 -0500

    Removed all useless fucntions from data/products

--- pkg/data/products.go
+++ pkg/data/products.go
@@ -1,12 +1,5 @@
 package data
 
-import (
-	"fmt"
-)
-
-// ErrorProductNotFound : Product specific errors
-var ErrorProductNotFound = fmt.Errorf("Product not found")
-
 // Player defines the structure for an API product.
 // Formatting done with json tags to the right. "-" : don't include when encoding to json
 type Player struct {
@@ -15,84 +8,16 @@ type Player struct {
 	Description string  `json:"description"`
 	Price       float32 `json:"price" validate:"gt=0"`
 	SKU         string  `json:"sku" validate:"required,sku"`
-	CreatedOn   string  `json:"-"`
-	UpdatedOn   string  `json:"-"`
-	DeletedOn   string  `json:"-"`
+	IP          string  `json:"ip" validate:"required,ip"`
 }
 
 // Products is a collection of Product
 type Products []*Player
 
-// All of these functions will become database calls in the future
-// GETTING PRODUCTS
-
-// GetProducts returns the list of products
-func GetProducts() Products {
-	return playerList
+// NewPlayer add the player to the game server queue
+func NewPlayer(player *Player) {
+	playerList = append(playerList, player)
 }
 
-// GetProductByID returns a single product with the given id
-func GetProductByID(id int) (*Player, error) {
-	index := findIndexByProductID(id)
-	if index == -1 {
-		return nil, ErrorProductNotFound
-	}
-	return playerList[index], nil
-}
-
-// UPDATING PRODUCTS
-
-// UpdateProduct updates the product specified in received JSON
-func UpdateProduct(product *Player) error {
-	index := findIndexByProductID(product.ID)
-	if index == -1 {
-		return ErrorProductNotFound
-	}
-	playerList[index] = product
-	return nil
-}
-
-// NewPlayer creates a new product
-func NewPlayer(product *Player) {
-	product.ID = getNextID()
-	playerList = append(playerList, product)
-}
-
-// DeleteProduct deletes the product with the given id
-func DeleteProduct(id int) error {
-	index := findIndexByProductID(id)
-	if index == -1 {
-		return ErrorProductNotFound
-	}
-
-	// This should not work, probably needs ':' after index+1. To test
-	playerList = append(playerList[:index], playerList[index+1])
-
-	return nil
-}
-
-// Returns the index of a product in the database
-// Returns -1 when no product is found
-func findIndexByProductID(id int) int {
-	for index, product := range playerList {
-		if product.ID == id {
-			return index
-		}
-	}
-	return -1
-}
-
-//////////////////////////////////////////////////////////////////////////////
-/////////////////////////// Fake database ///////////////////////////////////
-///// DB connection setup and docker file will be done in sprint 8 /////////
-///////////////////////////////////////////////////////////////////////////
-
-// Finds the maximum index of our fake database and adds 1
-func getNextID() int {
-	lastProduct := playerList[len(playerList)-1]
-	return lastProduct.ID + 1
-}
-
-// playerList is a hard coded list of products for this
-// example data source. Should be replaced by database connection
+// This will either be a database connection or an in memory array depending on how we end up handling game servers
 var playerList = []*Player{}
